c9b67ab3e6b1613316af69fbae9ac6724e40bc73
jesuslucifer <mit.com254@gmail.com>
2024-11-27 19:22:08 +0300

    changed diagram classes and change directory

--- task-3/4/src/Main.java
+++ task-3/4/src/Main.java
@@ -1,4 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        }
+        Hotel hotel = new Hotel();
+        List<Client> clients = new ArrayList<>();
+
+        hotel.addService("Massage", 100);
+        hotel.changeCostService("Massage", 30);
+
+        clients.add(new Client("321114", "Popov I.V."));
+        clients.add(new Client("123411", "Popova O.S."));
+        clients.add(new Client("431122", "Popov A.I."));
+        hotel.checkIntoRoom(clients);
+        clients.clear();
+
+        clients.add(new Client("545412", "Petrov E.A."));
+        clients.add(new Client("545412", "Petrova F.Q."));
+        hotel.checkIntoRoom(clients);
+        clients.clear();
+
+        clients.add(new Client("312122", "Frolov F.K."));
+        hotel.checkIntoRoom(clients);
+        clients.clear();
+
+        hotel.evictFromRoom(1);
+        hotel.evictFromRoom(3);
+        hotel.evictFromRoom(4);
+        hotel.evictFromRoom(5);
+
+        hotel.changeCostRoom(2, 99);
+
+        hotel.changeStatusRoom(4, "repair");
+
     }
+}
