061b720793080cfdc107dc51f16046ed6eeb757e
jesuslucifer <mit.com254@gmail.com>
2024-11-28 15:49:38 +0300

    add countStars in class Room

--- task-3/4/src/Room.java
+++ task-3/4/src/Room.java
@@ -4,15 +4,23 @@ import java.util.ArrayList;
 public class Room {
     private int roomNumber;
     private int cost;
+    private int countStars;
     private String status;
     private int capacity;
     private List<Client> clentList = new ArrayList<Client>();
 
     public Room(int roomNumber) {
         this.roomNumber = roomNumber;
-        this.cost = 20;
+        this.countStars = new java.util.Random().nextInt(5) + 1;
         this.status = "free";
         this.capacity = new java.util.Random().nextInt(3) + 1;
+        switch (countStars) {
+            case 1: cost = 10; break;
+            case 2: cost = 20; break;
+            case 3: cost = 30; break;
+            case 4: cost = 40; break;
+            case 5: cost = 50; break;
+        }
     }
 
     public int getRoomNumber() {
