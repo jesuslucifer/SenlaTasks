fe7d014749c6e7b229d69ab5db97f16d69568800
jesuslucifer <mit.com254@gmail.com>
2024-12-01 18:06:58 +0300

    add field serviceDate and methods getServiceDate, setServiceDate

--- task-3/4/src/Service.java
+++ task-3/4/src/Service.java
@@ -1,10 +1,14 @@
+import java.time.LocalDate;
+
 public class Service {
     private String serviceName;
     private int cost;
+    private LocalDate serviceDate;
 
     public Service(String serviceName, int cost) {
         this.serviceName = serviceName;
         this.cost = cost;
+        this.serviceDate = LocalDate.of(2020, 1, 1);;
     }
 
     public String getServiceName() {
@@ -17,5 +21,13 @@ public class Service {
 
     public int getCost() {
         return cost;
+    }
+
+    public LocalDate getServiceDate() {
+        return serviceDate;
+    }
+
+    public void setServiceDate(LocalDate serviceDate) {
+        this.serviceDate = serviceDate;
     }
 }
