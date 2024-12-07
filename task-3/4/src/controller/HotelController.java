0150a09f4cf11ef2b2235cc87678f2ca948153d2
jesuslucifer <mit.com254@gmail.com>
2024-12-07 18:14:51 +0300

    change Hotel and HotelController

--- task-3/4/src/controller/HotelController.java
+++ task-3/4/src/controller/HotelController.java
@@ -3,12 +3,9 @@ package controller;
 import model.Client;
 import model.Hotel;
 import model.Room;
-import model.RoomStatus;
 import model.Service;
 import view.HotelView;
-
 import java.time.LocalDate;
-import java.util.Comparator;
 import java.util.List;
 
 public class HotelController {
@@ -20,32 +17,9 @@ public class HotelController {
         this.view = view;
     }
 
-    public void checkIntoRoom(List<Client> buffClients, String dateCheckIn, String dateEvict) {
-        hotel.checkIntoRoom(buffClients, dateCheckIn, dateEvict);
-    }
-
-    public void evictFromRoom(int numberRoom) {
-        hotel.evictFromRoom(numberRoom);
-    }
-
-    public void changeStatusRoom(int roomNumber, RoomStatus status) {
-        hotel.changeStatusRoom(roomNumber, status);
-    }
-
-    public void changeCostRoom(int roomNumber, int cost) {
-        hotel.changeCostRoom(roomNumber, cost);
-    }
 
     public void addService(String serviceName, int cost) {
         hotel.addService(serviceName, cost);
-    }
-
-    public void changeCostService(String serviceName, int cost) {
-        hotel.changeCostService(serviceName, cost);
-    }
-
-    public void addServiceForClient(String serviceName, String fullName, String serviceDate) {
-        hotel.addServiceForClient(serviceName, fullName, serviceDate);
     }
 
     public List<Room> getListFreeRooms() {
@@ -68,113 +42,12 @@ public class HotelController {
         return hotel.formatDate(date);
     }
 
-    public void printRooms(String typeSort, String typeRoom) {
-        List<Room> list = getRooms();
-
-        if (typeRoom.equals("free"))
-        {
-            list = getListFreeRooms();
-        }
-
-        switch (typeSort) {
-            case "CapacityI":
-                list.sort(Comparator.comparing(Room::getCapacity));
-                break;
-            case "CapacityD":
-                list.sort(Comparator.comparing(Room::getCapacity).reversed());
-                break;
-            case "CostI":
-                list.sort(Comparator.comparing(Room::getCost));
-                break;
-            case "CostD":
-                list.sort(Comparator.comparing(Room::getCost).reversed());
-                break;
-            case "StarsI":
-                list.sort(Comparator.comparing(Room::getCountStars));
-                break;
-            case "StarsD":
-                list.sort(Comparator.comparing(Room::getCountStars).reversed());
-                break;
-            default:
-                list.sort(Comparator.comparing(Room::getRoomNumber));
-                break;
-        }
-
-        view.printRooms(list);
-    }
-
-    public void printInfoRoom(int roomNumber) {
-        view.printInfoRoom(roomNumber, getRooms());
-    }
-
     public void printCountFreeRoom() {
         view.printCountFreeRoom(getListFreeRooms());
     }
 
-    public void printCostPerRoom(String fullName) {
-        view.printCostPerRoom(fullName, getClients(), getRooms());
-    }
-
-    public void printHistoryRoom(int roomNumber) {
-        view.printHistoryRoom(roomNumber, getRooms());
-    }
-
-    public void printRoomFreeByDate(String date) {
-        view.printRoomFreeByDate(formatDate(date), getRooms());
-    }
-
     public void printCountClients() {
         view.printCountClients(getClients());
-    }
-
-    public void printClientList(String typeSort) {
-        List<Client> list = getClients();
-
-        switch (typeSort) {
-            case "AlphabetA":
-                list.sort(Comparator.comparing(Client::getFullName));
-                break;
-            case "AlphabetZ":
-                list.sort(Comparator.comparing(Client::getFullName).reversed());
-                break;
-            case "DateI":
-                list.sort(Comparator.comparing(Client::getDateEvict));
-                break;
-            case "DateD":
-                list.sort(Comparator.comparing(Client::getDateEvict).reversed());
-                break;
-            default:
-                break;
-        }
-
-        view.printClientList(getClients());
-    }
-
-    public void printClientServices(String fullName, String typeSort) {
-        for (Client client : getClients()) {
-            if (client.getFullName().equals(fullName)) {
-                System.out.println("Services " + client.getFullName() + ":");
-                List<Service> list = client.getServices();
-                switch (typeSort) {
-                    case "CostI":
-                        list.sort(Comparator.comparing(Service::getCost));
-                        break;
-                    case "CostD":
-                        list.sort(Comparator.comparing(Service::getCost).reversed());
-                        break;
-                    case "DateI":
-                        list.sort(Comparator.comparing(Service::getServiceDate));
-                        break;
-                    case "DateD":
-                        list.sort(Comparator.comparing(Service::getServiceDate).reversed());
-                        break;
-                    default:
-                        break;
-                }
-                view.printClientServices(list);
-                break;
-            }
-        }
     }
 
     public void printRoomAndService(String typeSort) {
--- task-3/4/src/model/Hotel.java
+++ task-3/4/src/model/Hotel.java
@@ -6,111 +6,23 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Hotel {
-    private List<Room> rooms = new ArrayList<>();
-    private List<Service> services = new ArrayList<>();
-    private List<Client> clients = new ArrayList<>();
-    private final int COUNT_ROOMS = 10;
+    private final List<Room> rooms = new ArrayList<>();
+    private final List<Service> services = new ArrayList<>();
+    private final List<Client> clients = new ArrayList<>();
 
     public Hotel() {
+        int COUNT_ROOMS = 10;
         for (int i = 0; i < COUNT_ROOMS; i++) {
             rooms.add(new Room(i));
         }
         System.out.println("The hotel is open, there are " + COUNT_ROOMS + " rooms available");
     }
 
-    //Заселяет клиентов в номер
-    public void checkIntoRoom(List<Client> buffClients, String dateCheckIn, String dateEvict) {
-        boolean found = false;
-        for (Room room : rooms) {
-            if (room.getCapacity() >= buffClients.size() && room.isFree()) {
-                for (Client client : buffClients) {
-                    client.setRoomNumber(room.getRoomNumber());
-                    client.setDateCheckIn(formatDate(dateCheckIn));
-                    client.setDateEvict(formatDate(dateEvict));
-                }
-                room.setClientList(buffClients);
-                room.setDateCheckIn(formatDate(dateCheckIn));
-                room.setDateEvict(formatDate(dateEvict));
-                clients.addAll(buffClients);
-                found = true;
-                room.setStatus(RoomStatus.BUSY);
-                System.out.println("The clients is accommodated in " + room.getRoomNumber() + " room");
-                break;
-            }
-        }
-        if (!found) {
-            System.out.println("There are no places in the hotel");
-        }
-    }
-
-    //Выселяет клиентов из номера
-    public void evictFromRoom(int roomNumber) {
-        for (Room room : rooms) {
-            if (room.getRoomNumber() == roomNumber) {
-                for (Client client : room.getClentList()) {
-                    room.addClientToHistory(client);
-                }
-                room.setStatus(RoomStatus.FREE);
-                room.setDateCheckIn(LocalDate.of(2020, 1, 1));
-                room.setDateEvict(LocalDate.of(2020, 1, 1));
-                clients.removeAll(room.getClentList());
-                room.getClentList().clear();
-            }
-        }
-        System.out.println("The guest has been evicted from the " + roomNumber + " room");
-    }
-
-    //Меняет статус номера
-    public void changeStatusRoom(int roomNumber, RoomStatus status) {
-        for (Room room : rooms) {
-            if (room.getRoomNumber() == roomNumber) {
-                room.setStatus(status);
-            }
-        }
-        System.out.println("The status of the room " + roomNumber + " has been changed to " + status);
-    }
-
-    //Меняет стоимость номера
-    public void changeCostRoom(int roomNumber, int cost) {
-        for (Room room : rooms) {
-            if (room.getRoomNumber() == roomNumber) {
-                room.setCost(cost);
-            }
-        }
-        System.out.println("The room cost has been changed to " + cost);
-    }
-
-    //Меняет стоимость услуги
-    public void changeCostService(String serviceName, int cost) {
-        for (Service service : services) {
-            if (service.getServiceName().equals(serviceName)) {
-                service.setCost(cost);
-            }
-        }
-        System.out.println("The service cost " + serviceName + " has been changed to " + cost);
-    }
-
-    //Добавляет услугу
     public void addService(String serviceName, int cost) {
         services.add(new Service(serviceName, cost));
         System.out.println("The service " + serviceName + " has been added to the hotel");
     }
 
-    //Добавляет услугу клиенту
-    public void addServiceForClient(String serviceName, String fullName, String serviceDate) {
-        for (Client client : clients) {
-            if (client.getFullName().equals(fullName)) {
-                for (Service service : services) {
-                    if (service.getServiceName().equals(serviceName)) {
-                        service.setServiceDate(formatDate(serviceDate));
-                        client.addService(service);
-                    }
-                }
-            }
-        }
-    }
-
-    //Возвращает список свободныхномеров
     public List<Room> getListFreeRooms() {
         List<Room> list = new ArrayList<>();
         for (Room room : rooms) {
@@ -133,7 +45,6 @@ public class Hotel {
         return services;
     }
 
-    //Форматирует дату из String в LocalDate
     public LocalDate formatDate(String date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         return LocalDate.parse(date, formatter);
