b00603a93ffc49073f4ee80768ef9027863b54b4
jesuslucifer <mit.com254@gmail.com>
2024-12-09 12:34:13 +0300

    delete method addService

--- task-3/4/src/view/MenuHotel.java
+++ task-3/4/src/view/MenuHotel.java
@@ -16,32 +16,21 @@ public class MenuHotel {
         boolean flag = true;
         while (flag) {
             System.out.println("\tMenu Hotel");
-            System.out.println("1. Add service");
-            System.out.println("2. Print count free rooms");
-            System.out.println("3. Print count clients");
-            System.out.println("4. Print rooms and services");
-            System.out.println("5. Back");
+            System.out.println("1. Print count free rooms");
+            System.out.println("2. Print count clients");
+            System.out.println("3. Print rooms and services");
+            System.out.println("4. Back");
             System.out.println("0. Exit");
 
             switch (sc.nextInt()) {
-                case 1 -> addService();
-                case 2 -> hotelController.printCountFreeRoom();
-                case 3 -> hotelController.printCountClients();
-                case 4 -> printRoomAndService();
-                case 5 -> flag = false;
-                case 6 -> System.exit(0);
+                case 1 -> hotelController.printCountFreeRoom();
+                case 2 -> hotelController.printCountClients();
+                case 3 -> printRoomAndService();
+                case 4 -> flag = false;
+                case 0 -> System.exit(0);
             }
 
         }
-    }
-
-    public void addService() {
-        System.out.println("Enter service name: ");
-        String serviceName = new Scanner(System.in).nextLine();
-        System.out.println("Enter service cost: ");
-        int serviceCost = new Scanner(System.in).nextInt();
-
-        hotelController.addService(serviceName, serviceCost);
     }
 
     public void printRoomAndService() {
