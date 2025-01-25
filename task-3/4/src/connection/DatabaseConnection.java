0586673e1541c27d333ce81401ad75413c418c8c
jesuslucifer <mit.com254@gmail.com>
2025-01-25 12:58:49 +0300

    add DatabaseConnection

---
+++ task-3/4/src/connection/DatabaseConnection.java
@@ -0,0 +1,33 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private Connection connection;
+    private static DatabaseConnection instance;
+
+    private static final String URL ="jdbc:mysql://localhost:3306/";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    private DatabaseConnection() {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
