1313668387a578f9c1cff4c59471f0cae7369efd
jesuslucifer <mit.com254@gmail.com>
2025-02-14 23:59:33 +0300

    change methods JDBC on Hibernate in RoomDAO

--- task-3/4/src/main/java/dao/RoomDAO.java
+++ task-3/4/src/main/java/dao/RoomDAO.java
@@ -1,8 +1,13 @@
 package dao;
 
 import connection.DatabaseConnection;
+import jakarta.persistence.Query;
 import lombok.extern.slf4j.Slf4j;
 import model.Room;
+import model.RoomStatus;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import util.HibernateUtil;
 
 import java.sql.Connection;
 import java.sql.Date;
@@ -22,76 +27,38 @@ public class RoomDAO implements IGenericDAO<Room> {
 
     @Override
     public void create(Room room) {
-        String query = "INSERT INTO Rooms(roomNumber, cost, status, capacity, dateCheckIn, dateEvict, countStars)  VALUES (?, ?, ?, ?, ?, ?, ?)";
-        printLogQuery(query);
-        try {
-            connection = DatabaseConnection.getInstance().getConnection();
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setInt(1, room.getRoomNumber());
-            statement.setInt(2, room.getCost());
-            statement.setString(3, room.getStatus().toString());
-            statement.setInt(4, room.getCapacity());
-            statement.setNull(5, java.sql.Types.DATE);
-            statement.setNull(6, java.sql.Types.DATE);
-            statement.setInt(7, room.getCountStars());
-            statement.execute();
-            log.info("Room created ID {}", room.getId());
-        } catch (SQLException e) {
-            log.error("Error creating room: ", e);
+        Transaction tx = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            session.save(room);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            log.error("Error creating room", e);
         }
     }
 
-//    @Override
-//    public Room read(int id) {
-//        return null;
-//    }
-
     @Override
     public Room read(int roomNumber) {
-        String query = "SELECT * FROM Rooms WHERE roomNumber = ?";
-        printLogQuery(query);
-        try {
-            connection = DatabaseConnection.getInstance().getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1, roomNumber);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                log.info("Room read ID {}", roomNumber);
-                return toRoom(resultSet);
-            }
-        } catch (SQLException e) {
-            log.error("Error reading room: ", e);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Room.class, roomNumber);
         }
-        return null;
     }
 
     @Override
     public void update(Room room) {
-        String query = "UPDATE Rooms SET cost = ?, status = ?, dateCheckIn = ?, dateEvict = ?, lockedChangeStatus = ?, " +
-                "countRecordsHistory = ? WHERE roomNumber = ?";
-        printLogQuery(query);
-        try {
-            connection = DatabaseConnection.getInstance().getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1, room.getCost());
-            preparedStatement.setString(2, room.getStatus().toString());
-            if (room.getDateCheckIn() != null) {
-                preparedStatement.setDate(3, Date.valueOf(room.getDateCheckIn()));
-            } else {
-                preparedStatement.setNull(3, java.sql.Types.DATE);
+        Transaction tx = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            session.update(room);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
             }
-            if (room.getDateEvict() != null) {
-                preparedStatement.setDate(4, Date.valueOf(room.getDateEvict()));
-            } else {
-                preparedStatement.setNull(4, java.sql.Types.DATE);
-            }
-            preparedStatement.setBoolean(5, room.getLockedChangeStatus());
-            preparedStatement.setInt(6, room.getCountRecordsHistory());
-            preparedStatement.setInt(7, room.getRoomNumber());
-            preparedStatement.executeUpdate();
-            log.info("Room updated ID {}", room.getId());
-        } catch (SQLException e) {
-            log.error("Error updating room: ", e);
+            log.error("Error updating room", e);
         }
     }
 
@@ -102,46 +69,41 @@ public class RoomDAO implements IGenericDAO<Room> {
 
     @Override
     public List<Room> findAll() {
-        String query = "SELECT * FROM Rooms";
-        return getRooms(query);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("FROM Room").list();
+        }
     }
 
-    private List<Room> getRooms(String query) {
-        List<Room> rooms = new ArrayList<>();
-        printLogQuery(query);
-        try {
-            connection = DatabaseConnection.getInstance().getConnection();
-            PreparedStatement statement = connection.prepareStatement(query);
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                rooms.add(toRoom(resultSet));
-                log.info("Room ID {}", rooms.getLast().getId());
+    private List<Room> getRooms(String queryType, String typeSort) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            if (queryType.equals("ALL")) {
+                String query = "FROM Room WHERE status IN (:status1, :status2, :status3) ORDER BY " + typeSort;
+                return session.createQuery(query)
+                        .setParameter("status1", RoomStatus.FREE)
+                        .setParameter("status2", RoomStatus.BUSY)
+                        .setParameter("status3", RoomStatus.REPAIRED)
+                        .list();
+            } else {
+                String query = "FROM Room WHERE status = :status ORDER BY " + typeSort;
+                return session.createQuery(query)
+                        .setParameter("status", RoomStatus.FREE)
+                        .list();
             }
-            log.info("Rooms found");
-        } catch (SQLException e) {
-            log.error("Error reading rooms: ", e);
+        } catch (Exception e) {
+            log.error("Error getting rooms", e);
+            throw new RuntimeException(e);
         }
-        return rooms;
     }
 
     public List<Room> findByStatus(String status) {
-        List<Room> rooms = new ArrayList<>();
-        String query = "SELECT * FROM Rooms WHERE status = ?";
-        printLogQuery(query);
-        try {
-            connection = DatabaseConnection.getInstance().getConnection();
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setString(1, status);
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                rooms.add(toRoom(resultSet));
-                log.info("Rooms by status {} ID {}", rooms.getLast().getStatus(), rooms.getLast().getId());
-            }
-            log.info("Rooms found by status");
-        } catch (SQLException e) {
-            log.error("Error found rooms by status: ", e);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("FROM Room WHERE status = :status")
+                    .setParameter("status", status)
+                    .list();
+        } catch (Exception e) {
+            log.error("Error getting rooms", e);
+            throw new RuntimeException(e);
         }
-        return rooms;
     }
 
     public Room toRoom(ResultSet resultSet) throws SQLException {
@@ -168,36 +130,19 @@ public class RoomDAO implements IGenericDAO<Room> {
     }
 
     public List<Room> findFreeByDate(LocalDate date) {
-        List<Room> rooms = new ArrayList<>();
-        String query = "SELECT * FROM Rooms WHERE status != 'REPAIRED' AND ((dateCheckIn IS NULL OR dateEvict IS NULL) OR dateEvict < ?)";
-        printLogQuery(query);
-        try {
-            connection = DatabaseConnection.getInstance().getConnection();
-            connection.setAutoCommit(false);
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setDate(1, Date.valueOf(date));
-            ResultSet resultSet = preparedStatement.executeQuery();
-            connection.commit();
-            connection.setAutoCommit(true);
-            while (resultSet.next()) {
-                rooms.add(toRoom(resultSet));
-                log.info("Rooms free by date {} ID {}", date, rooms.getLast().getId());
-            }
-            log.info("Rooms free by date found");
-        } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-            log.error("Error found room free by date: ", e);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("FROM Room WHERE status != :status AND ((dateCheckIn IS NULL OR dateEvict IS NULL) OR dateEvict < :date)")
+                    .setParameter("status", RoomStatus.REPAIRED)
+                    .setParameter("date", date)
+                    .list();
+        } catch (Exception e) {
+            log.error("Error getting rooms free by date ", e);
+            throw new RuntimeException(e);
         }
-        return rooms;
     }
 
-    public List<Room> findWithSort(String typeRoom, String typeSort) {
-        String query = "SELECT * FROM Rooms WHERE status IN (" + typeRoom + ") ORDER BY " + typeSort;
-        return getRooms(query);
+    public List<Room> findWithSort(String typeQuery, String typeSort) {
+        return getRooms(typeQuery, typeSort);
     }
 
     public void printLogQuery(String query) {
