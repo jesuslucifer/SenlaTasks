55cbaa664d210ca7fff42e45d89fdc445e5b0a78
jesuslucifer <mit.com254@gmail.com>
2025-02-15 12:44:45 +0300

    change methods JDBC on Hibernate in ClientDAO

--- task-3/4/src/main/java/dao/ClientDAO.java
+++ task-3/4/src/main/java/dao/ClientDAO.java
@@ -4,6 +4,9 @@ import connection.DatabaseConnection;
 import lombok.extern.slf4j.Slf4j;
 import model.Client;
 import model.Service;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import util.HibernateUtil;
 
 import java.sql.Connection;
 import java.sql.Date;
@@ -22,36 +25,31 @@ public class ClientDAO implements IGenericDAO<Client> {
 
     @Override
     public void create(Client client) {
-        String query = "INSERT INTO Clients (fullName, roomNumber, dateCheckIn, dateEvict) VALUES (?, ?, ?, ?)";
-        printLogQuery(query);
-        try {
-            Connection connection = DatabaseConnection.getInstance().getConnection();
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setString(1, client.getFullName());
-            statement.setInt(2, client.getRoomNumber());
-            statement.setDate(3, Date.valueOf(client.getDateCheckIn()));
-            statement.setDate(4, Date.valueOf(client.getDateEvict()));
-            statement.executeUpdate();
-            log.info("Client created ID {}", client.getId());
-        } catch (SQLException e) {
+        Transaction tx = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            session.save(client);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
             log.error("Error creating client", e);
         }
     }
 
     @Override
     public Client read(int id) {
-        String query = "SELECT * FROM Clients WHERE id = ?";
-        printLogQuery(query);
-        try {
-            Connection connection = DatabaseConnection.getInstance().getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                log.info("Client read ID {}", id);
-                return toClient(resultSet);
+        Transaction tx = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            Client client = session.get(Client.class, id);
+            tx.commit();
+            return client;
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
             }
-        } catch (SQLException e) {
             log.error("Error reading client", e);
         }
         return null;
@@ -59,53 +57,42 @@ public class ClientDAO implements IGenericDAO<Client> {
 
     @Override
     public void update(Client client) {
-        String query = "UPDATE Clients SET occupied = ? WHERE id = ?";
-        printLogQuery(query);
-        try {
-            Connection connection = DatabaseConnection.getInstance().getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setBoolean(1, client.getOccupied());
-            preparedStatement.setInt(2, client.getId());
-            preparedStatement.executeUpdate();
-            log.info("Client updated ID {}", client.getId());
-        } catch (SQLException e) {
-            log.error("Error updating client: ", e);
+        Transaction tx = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            session.update(client);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            log.error("Error updating client", e);
         }
     }
 
     @Override
     public void delete(Client client) {
-        String query = "DELETE FROM Clients WHERE id = ?";
-        printLogQuery(query);
-        try {
-            Connection connection = DatabaseConnection.getInstance().getConnection();
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setInt(1, client.getId());
-            statement.executeUpdate();
-            log.info("Client deleted ID {}", client.getId());
-        } catch (SQLException e) {
-            log.error("Error deleting client: ", e);
+        Transaction tx = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            tx = session.beginTransaction();
+            session.delete(client);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            log.error("Error deleting client", e);
         }
     }
 
     @Override
     public List<Client> findAll() {
-        List<Client> clients = new ArrayList<>();
-        String query = "SELECT * FROM Clients";
-        printLogQuery(query);
-        try {
-            Connection connection = DatabaseConnection.getInstance().getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                clients.add(toClient(resultSet));
-                log.info("Client ID {}", clients.getLast().getId());
-            }
-            log.info("Clients found");
-        } catch (SQLException e) {
-            log.error("Error finding clients: ", e);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("FROM Client").list();
+        } catch (Exception e) {
+            log.error("Error finding all clients", e);
+            throw new RuntimeException(e);
         }
-        return clients;
     }
 
     public Client findFullName(String fullName) {
@@ -125,23 +112,14 @@ public class ClientDAO implements IGenericDAO<Client> {
     }
 
     public List<Client> findInRoom(int roomNumber) {
-        List<Client> clients = new ArrayList<>();
-        String query = "SELECT * FROM Clients WHERE roomNumber = ? and occupied = true";
-        printLogQuery(query);
-        try {
-            Connection connection = DatabaseConnection.getInstance().getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1, roomNumber);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                clients.add(toClient(resultSet));
-                log.info("Client ID {} in room {}", clients.getLast().getId(), roomNumber);
-            }
-            log.info("Clients in room ID {} found", roomNumber);
-        } catch (SQLException e) {
-            log.error("Error finding clients in room: ", e);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("FROM Client WHERE roomNumber = :roomNumber", Client.class)
+                    .setParameter("roomNumber", roomNumber)
+                    .list();
+        } catch (Exception e) {
+            log.error("Error clients in room", e);
+            throw new RuntimeException(e);
         }
-        return clients;
     }
 
     public void addService(Client client, Service service, LocalDate date) {
@@ -198,43 +176,26 @@ public class ClientDAO implements IGenericDAO<Client> {
     }
 
     public List<Client> findAllWithSort(String typeSort) {
-        List<Client> clients = new ArrayList<>();
-        String query = "SELECT * FROM Clients WHERE occupied = true ORDER BY " + typeSort + " ";
-        printLogQuery(query);
-        try {
-            Connection connection = DatabaseConnection.getInstance().getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                clients.add(toClient(resultSet));
-                log.info("Client with sort {} ID {}", typeSort, clients.getLast().getId());
-            }
-            log.info("Clients found with sort {}", typeSort);
-        } catch (SQLException e) {
-            log.error("Error finding clients with sort: ", e);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            String query = "FROM Client WHERE occupied = true ORDER BY " + typeSort;
+            return session.createQuery(query).list();
+        } catch (Exception e) {
+            log.error("Error finding all clients", e);
+            throw new RuntimeException(e);
         }
-        return clients;
     }
 
     public List<Client> printHistory(int roomNumber, int countRecordsHistory) {
-        List<Client> clients = new ArrayList<>();
-        String query = "SELECT * FROM Clients WHERE roomNumber = ? AND occupied = false ORDER BY dateEvict DESC LIMIT ?";
-        printLogQuery(query);
-        try {
-            Connection connection = DatabaseConnection.getInstance().getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1, roomNumber);
-            preparedStatement.setInt(2, countRecordsHistory);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                clients.add(toClient(resultSet));
-                log.info("Client ID {}", clients.getLast().getId());
-            }
-            log.info("Successfully printed history");
-        } catch (SQLException e) {
-            log.error("Error printing history: ", e);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            String query = "FROM Client WHERE roomNumber = :roomNumber AND occupied = false ORDER BY dateEvict DESC LIMIT :limit";
+            return session.createQuery(query)
+                    .setParameter("roomNumber", roomNumber)
+                    .setParameter("limit", countRecordsHistory)
+                    .list();
+        } catch (Exception e) {
+            log.error("Error finding all clients", e);
+            throw new RuntimeException(e);
         }
-        return clients;
     }
 
     public void printLogQuery(String query) {
