e8a620c950273b9faec021ad0bf5e76939f148fa
jesuslucifer <mit.com254@gmail.com>
2025-02-16 14:38:50 +0300

    change method isHotelInitialized

--- task-3/4/src/main/java/controller/MainController.java
+++ task-3/4/src/main/java/controller/MainController.java
@@ -1,19 +1,12 @@
 package controller;
 
-import connection.DatabaseConnection;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import util.HibernateUtil;
 import view.Menu;
 import model.Hotel;
-
-import javax.imageio.IIOException;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 @Slf4j
 public class MainController {
@@ -47,28 +40,15 @@ public class MainController {
     }
 
     public boolean isHotelInitialized() {
-        String query = "SELECT COUNT(*) FROM Rooms";
-        try {
-            Connection connection = DatabaseConnection.getInstance().getConnection();
-            PreparedStatement statement = connection.prepareStatement(query);
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Long> query = session.createQuery("SELECT COUNT(*) FROM Room");
+            if (query.uniqueResult() > 0) {
                 log.info("Hotel initialized");
-                return resultSet.getInt(1) > 0;
+                return true;
             }
-        } catch (SQLException e) {
+        } catch (HibernateException e) {
             log.error("Error initializing Hotel", e);
         }
         return false;
-    }
-
-    public Hotel loadHotelFromSave() {
-        try (ObjectInputStream os = new ObjectInputStream(new FileInputStream("task-3/4/src/resources/save.dat"))) {
-            return (Hotel) os.readObject();
-        } catch (FileNotFoundException | IIOException | ClassNotFoundException e) {
-            return new Hotel();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
